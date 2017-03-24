5268894d935921d57546cf2ef227cfd021f93266
mzilic <mzilic@mono.hr>
2017-03-24 15:26:01 +0100

    Add new html classes

--- src/templates.js
+++ src/templates.js
@@ -17,11 +17,11 @@ angular.module("/angular-time-picker.tpl.html", []).run(["$templateCache", funct
     "			<span class=\"angular-time-picker-float--right\">\n" +
     "				<!-- `browser-default` class is being used as materializecss framework override default select css-->\n" +
     "				<!-- Thus to prevent this, adding a class. Materializecss is a famous framework for Material Design. -->\n" +
-    "				<select ng-model=\"startingHour\" class=\"browser-default\">\n" +
+    "				<select ng-model=\"startingHour\" class=\"input__field input--select w--3\">\n" +
     "					<option ng-repeat=\"option in startingTimeHoursRange\" ng-disabled=\"option.disabled\" value=\"{{option.value}}\">{{option.name}}</option>\n" +
     "				</select>\n" +
     "				:\n" +
-    "				<select ng-model=\"startingMinute\" class=\"browser-default\">\n" +
+    "				<select ng-model=\"startingMinute\" class=\"input__field input--select w--3\">\n" +
     "					<option ng-repeat=\"option in startingTimeHMinutesRange\" ng-disabled=\"option.disabled\" value=\"{{option.value}}\">{{option.name}}</option>\n" +
     "				</select>\n" +
     "			</span>\n" +
@@ -29,19 +29,19 @@ angular.module("/angular-time-picker.tpl.html", []).run(["$templateCache", funct
     "		<div class=\"angular-time-picker-push--top\" ng-show=\"!noRange\">\n" +
     "			End:\n" +
     "			<span class=\"angular-time-picker-float--right\">\n" +
-    "				<select ng-model=\"endingHour\" class=\"browser-default\">\n" +
+    "				<select ng-model=\"endingHour\" class=\"input__field input--select w--3\">\n" +
     "					<option ng-repeat=\"option in endingTimeHoursRange\" ng-disabled=\"option.disabled\" value=\"{{option.value}}\">{{option.name}}</option>\n" +
     "				</select>\n" +
     "				:\n" +
-    "				<select ng-model=\"endingMinute\" class=\"browser-default\">\n" +
+    "				<select ng-model=\"endingMinute\" class=\"input__field input--select w--3\">\n" +
     "					<option ng-repeat=\"option in endingTimeHMinutesRange\" ng-disabled=\"option.disabled\" value=\"{{option.value}}\">{{option.name}}</option>\n" +
     "				</select>\n" +
     "			</span>\n" +
     "		</div>\n" +
     "		<div class=\"angular-time-picker-push--top\">\n" +
-    "			<button type=\"button\" ng-click=\"resetToOriginalTimeSettings()\">Reset</button>\n" +
-    "			<button type=\"button\" class=\"angular-time-picker-push-half--left  angular-time-picker-float--right  angular-time-picker-apply-btn\" ng-click=\"applyTimeRangeFilter()\">Apply</button>\n" +
-    "			<button type=\"button\" class=\"angular-time-picker-push-half--left  angular-time-picker-float--right  angular-time-picker-cancel-btn\" ng-click=\"closeTimeFilterDropdown()\">Cancel</button>\n" +
+    "			<button type=\"button\" class=\"btn--primary btn--med\" ng-click=\"resetToOriginalTimeSettings()\">Reset</button>\n" +
+    "			<button type=\"button\" class=\"angular-time-picker-push-half--left  angular-time-picker-float--right  angular-time-picker-apply-btn btn btn--primary btn--med\" ng-click=\"applyTimeRangeFilter()\">Apply</button>\n" +
+    "			<button type=\"button\" class=\"angular-time-picker-push-half--left  angular-time-picker-float--right  angular-time-picker-cancel-btn btn--primary btn--med\" ng-click=\"closeTimeFilterDropdown()\">Cancel</button>\n" +
     "		</div>\n" +
     "	</div>\n" +
     "</div>\n" +
